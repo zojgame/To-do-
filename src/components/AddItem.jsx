ae998f25eb2abf619fa4072cd59855de90fc0ae6
Danil <zogame002@gmail.com>
2023-12-17 22:38:26 +0500

    com

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,7 +1,7 @@
-import { React, useState } from 'react';
+import { React, useState } from "react";
 
 const AddItem = ({ handleClickAdd, isNight }) => {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
   // Устанавливает текущее значение input в переменную value при любом изменении в input
   const handleChange = (e) => {
@@ -9,31 +9,31 @@ const AddItem = ({ handleClickAdd, isNight }) => {
   };
 
   // Обработчик нажатия клавиши Enter в input при добавлении задачи
-  const handleKey = (e) => {
-    if (e.key === 'Enter' && value.length > 0) {
-      setValue('');
-      const task = {
-        status: false,
-        text: value,
-        // ID генерируется через дату (используется время создания задачи). Это обеспечивает уникальность id
-        id: Number(new Date().getTime()),
-      };
-      handleClickAdd(task);
-    }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+    const task = {
+      status: false,
+      text: value,
+      // ID генерируется через дату (используется время создания задачи). Это обеспечивает уникальность id
+      id: Number(new Date().getTime()),
+    };
+    handleClickAdd(task);
   };
 
   return (
-    <form className="new-task" name="addTask">
+    <form className="new-task" name="addTask" onSubmit={handleOnSubmit}>
       <input
         className={`new-task__input ${
-          isNight && 'new-task__input_night-theme_active'
+          isNight && "new-task__input_night-theme_active"
         }`}
         placeholder="Добавить новую задачу"
         name="addNewTask"
         id="addNewTask"
-        value={value}
+        defaultValue={value}
         onChange={handleChange}
-        onKeyDown={handleKey}
+        // onKeyDown={handleKey}
         type="text"
         minLength={1}
         maxLength={30}
